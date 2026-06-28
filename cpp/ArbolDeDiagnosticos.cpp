80cc5a1f3bbc2d7248a0a94224d44e7c7c0cefc1
ArielValenzuela1 <arielsebastian.valenzuela@estudiantes.unahur.edu.ar>
2026-06-27 21:09:37 -0300

    inorder y altura en ArbolDeDiagnosticos

--- cpp/ArbolDeDiagnosticos.cpp
+++ cpp/ArbolDeDiagnosticos.cpp
@@ -1,4 +1,8 @@
 #include "../header/ArbolDeDiagnosticos.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
 
 ArbolDeDiagnosticos::ArbolDeDiagnosticos()
 {
@@ -19,4 +23,58 @@ bool ArbolDeDiagnosticos::esMenor(Diagnostico* primero, Diagnostico* segundo)
         return false;
 
     return primero->getNombre() < segundo->getNombre();
+}
+
+Diagnostico* ArbolDeDiagnosticos::insertarRec(Diagnostico* actual, Diagnostico* nuevo)
+{
+    if (actual == nullptr)
+        return nuevo;
+
+    if (esMenor(nuevo, actual))
+        actual->setIzquierdo(insertarRec(actual->getIzquierdo(), nuevo));
+    else
+        actual->setDerecho(insertarRec(actual->getDerecho(), nuevo));
+
+    return actual;
+}
+
+void ArbolDeDiagnosticos::insertar(Diagnostico* diagnostico)
+{
+    raiz = insertarRec(raiz, diagnostico);
+}
+
+void ArbolDeDiagnosticos::inorder(Diagnostico* actual)
+{
+    if (actual == nullptr)
+        return;
+
+    inorder(actual->getIzquierdo());
+
+    cout << actual->getNombre()
+         << " | Frecuencia: "
+         << actual->getFrecuencia()
+         << endl;
+
+    inorder(actual->getDerecho());
+}
+
+void ArbolDeDiagnosticos::listarDiagnosticos()
+{
+    inorder(raiz);
+}
+
+int ArbolDeDiagnosticos::alturaRec(Diagnostico* actual)
+{
+    if (actual == nullptr)
+        return 0;
+
+    int izquierda = alturaRec(actual->getIzquierdo());
+    int derecha = alturaRec(actual->getDerecho());
+
+    return 1 + max(izquierda, derecha);
+}
+
+int ArbolDeDiagnosticos::altura()
+{
+    return alturaRec(raiz);
 }
